34d4aae3414897e3179ee5d7df1d4a8fcb80f053
Sbn6 <serbantoader123@gmail.com>
2022-05-21 16:39:47 +0300

    materii semestre

--- src/Learn1.js
+++ src/Learn1.js
@@ -1,6 +1,6 @@
 const LearnSetup = () => {
 
-    var currentclass = 0;
+    var currentclass = 0, currentsemester = 0, currentobjects = 2;
 
     const activate9 = () => {
         currentclass = 9;
@@ -30,7 +30,32 @@ const LearnSetup = () => {
         document.getElementById('11').classList.remove('active');
         document.getElementById('12').classList.add('active');
     }
-
+    const activateSemester1 = () => {
+        currentsemester = 1;
+        document.getElementById('sem1').classList.add('active');
+        document.getElementById('sem2').classList.remove('active');
+    }
+    const activateSemester2 = () => {
+        currentsemester = 2;
+        document.getElementById('sem1').classList.remove('active');
+        document.getElementById('sem2').classList.add('active');
+    }
+    const activatemat3 = () => {
+        currentobjects++;
+        document.getElementById('mat3').classList.add('active');
+    }
+    const activatemat4 = () => {
+        currentobjects++;
+        document.getElementById('mat4').classList.add('active');
+    }
+    const activatemat5 = () => {
+        currentobjects++;
+        document.getElementById('mat5').classList.add('active');
+    }
+    const activatemat6 = () => {
+        currentobjects++;
+        document.getElementById('mat6').classList.add('active');
+    }
     return (  
         <div className="learn1">
 
@@ -39,19 +64,43 @@ const LearnSetup = () => {
                 <h5 className="card-title">Setup</h5>
                 <p className="card-text">Pentru a-ti genera calendarul de invatare, va trebui sa ne oferi cateva informatii.</p>
                     <div className="card" style={{width: "10rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Clasa</h5>
-                        <p className="card-text"></p>
-                        <div className="list-group">
-                        <button type="button" className="list-group-item list-group-item-action" onClick={activate9} id="9">9</button>
-                        <button type="button" className="list-group-item list-group-item-action" onClick={activate10} id="10">10</button>
-                        <button type="button" className="list-group-item list-group-item-action" onClick={activate11} id="11">11</button>
-                        <button type="button" className="list-group-item list-group-item-action" onClick={activate12} id="12">12</button>
+                        <div className="card-body">
+                            <h5 className="card-title">Clasa</h5>
+                            <p className="card-text"></p>
+                            <div className="list-group">
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activate9} id="9">9</button>
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activate10} id="10">10</button>
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activate11} id="11">11</button>
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activate12} id="12">12</button>
+                            </div>
                         </div>
                     </div>
+                    <div className="card" style={{width: "10rem", marginTop:"-238px", marginLeft:"11rem"}}>
+                        <div className="card-body">
+                            <h5 className="card-title">Semestrul</h5>
+                            <p className="card-text"></p>
+                            <div className="list-group">
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activateSemester1} id="sem1">1</button>
+                                <button type="button" className="list-group-item list-group-item-action" onClick={activateSemester2} id="sem2">2</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="card position-absolute top-0" style={{width:"10rem", marginLeft:"22.7rem", marginTop:"113px"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Materii</h5>
+                        <p className="card-text"></p>
+                        <div className="list-group">
+                            <button type="button" className="list-group-item list-group-item-action active" id="mat1">Lb. Romana</button>
+                            <button type="button" className="list-group-item list-group-item-action active" id="mat2">Matematica</button>
+                            <button type="button" className="list-group-item list-group-item-action" id="mat3" onClick={activatemat3}>Informatica</button>
+                            <button type="button" className="list-group-item list-group-item-action" id="mat4" onClick={activatemat4}>Fizica</button>
+                            <button type="button" className="list-group-item list-group-item-action" id="mat5" onClick={activatemat5}>Biologie</button>
+                            <button type="button" className="list-group-item list-group-item-action" id="mat6" onClick={activatemat6}>Chimie</button>
+                        </div>
+                    </div>
+                </div>    
+            </div>            
         </div>
     );
 }
